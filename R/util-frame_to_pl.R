6c06e0a5e87bfb9650e55bc189e4566b4fe746e0
ztgroton <ztgroton@gmail.com>
2023-02-06 20:40:57 -0500

    Created new utility functions for validating format of 'pos_list' components.

--- R/util-frame_to_pl.R
+++ R/util-frame_to_pl.R
@@ -317,3 +317,259 @@ gen_pl_records <- function(data, ...) {
   return(result)
 
 }
+
+#' Validate if an input object is 'pl_map'
+#'
+#' @param obj S3 Object
+#'
+#' @return R Object
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' test <- valid_pl_map(s3_obj)
+#' }
+valid_pl_map <- function(obj) {
+
+  # Validate Inputs
+  if (missing(obj)) {stop("`obj` is missing in call to `valid_pl_map`")}
+
+  # Initialize Empty Character Vector for Error Messages
+  err <- vector(mode = 'character')
+
+  # Validate Input Expectations
+
+  # * is list?
+  cat("is list?...")
+  tictoc::tic()
+  if (!isTRUE(is.list(obj))) {
+    msg <- "`obj` must be a list"
+    err <- c(msg, err)
+  }
+  tictoc::toc()
+
+  # * does list have names?
+  cat("does list have names?...")
+  tictoc::tic()
+  obj_names <- names(obj)
+  obj_names_char <- isTRUE(is.character(obj_names))
+  obj_names_na <- purrr::map_lgl(obj_names, function(x){isTRUE(is.na(x))})
+  obj_names_null <- purrr::map_lgl(obj_names, function(x){isTRUE(is.null(x))})
+  obj_names_empty <- isTRUE(any(obj_names_na)) || isTRUE(any(obj_names_null))
+  if (!isTRUE(obj_names_char) && !isTRUE(obj_names_empty)) {
+    msg <- "`names(obj)` must be character vector with no NA/NULL elements"
+    err <- c(msg, err)
+  }
+  tictoc::toc()
+
+  # Conditionally return 'err', if any errors at this point
+  if (isTRUE(length(err) > 0)) {return(err)}
+
+  # * are list elements data.frames?
+  cat("are list elements data.frames?...")
+  tictoc::tic()
+  obj_elem_is_df <- purrr::map_lgl(obj, function(x){isTRUE(is.data.frame(x))})
+  if (!isTRUE(all(obj_elem_is_df))) {
+    msg <- "`obj` must only contain data.frames as elements"
+    err <- c(msg, err)
+  }
+  tictoc::toc()
+
+  # * do all list elements have correct colnames?
+  cat("do all list elements have correct colnames?...")
+  tictoc::tic()
+  elem_colnames <- c('val', 'index')
+  elem_valid_colnames <- purrr::map_lgl(obj, function(x){
+    isTRUE(identical(sort(colnames(x)), sort(elem_colnames)))
+  })
+  if (!isTRUE(all(elem_valid_colnames))) {
+    msg <- "`colnames(elem)` must equal `c('val', 'index')`"
+    err <- c(msg, err)
+  }
+  tictoc::toc()
+
+  # Conditionally return 'err', if any errors at this point
+  if (isTRUE(length(err) > 0)) {return(err)}
+
+  # * does each `obj$elem$index` make sense with `obj$elem$val`?
+  cat("does each 'obj$elem$index' make sense with 'obj$elem$val'?...")
+  tictoc::tic()
+  index_val_match <- purrr::map_lgl(obj, function(x) {
+    unq_val_cnt <- length(unique(x$val))
+    res <- isTRUE(identical(as.integer(sort(1:unq_val_cnt)), as.integer(sort(x$index))))
+    return(res)
+  })
+  if (!isTRUE(all(index_val_match))) {
+    msg <- "`obj` elements must have valid column values"
+    err <- c(msg, err)
+  }
+  tictoc::toc()
+
+  # Return Final Output
+  if (isTRUE(length(err) == 0)) {return(obj)}
+  else {return(err)}
+
+}
+
+#' Validate if an input object is 'pl_index'
+#'
+#' @param obj S3 Object
+#'
+#' @return R Object
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' test <- valid_pl_index(obj)
+#' }
+valid_pl_index <- function(obj) {
+
+  # Validate Inputs
+  if (missing(obj)) {stop("`obj` is missing in call to `valid_pl_index`")}
+
+  # Initialize Empty Character Vector for Error Messages
+  err <- vector(mode = 'character')
+
+  # Validate Input Expectations
+
+  # * is list?
+  cat("is list?...")
+  tictoc::tic()
+  if (!isTRUE(is.list(obj))) {
+    msg <- "`obj` must be a list"
+    err <- c(msg, err)
+  }
+  tictoc::toc()
+
+  # * does list have names?
+  cat("does list have names?...")
+  tictoc::tic()
+  obj_names <- names(obj)
+  obj_names_char <- isTRUE(is.character(obj_names))
+  obj_names_na <- purrr::map_lgl(obj_names, function(x){isTRUE(is.na(x))})
+  obj_names_null <- purrr::map_lgl(obj_names, function(x){isTRUE(is.null(x))})
+  obj_names_empty <- isTRUE(any(obj_names_na)) || isTRUE(any(obj_names_null))
+  if (!isTRUE(obj_names_char) && !isTRUE(obj_names_empty)) {
+    msg <- "`names(obj)` must be character vector with no NA/NULL elements"
+    err <- c(msg, err)
+  }
+  tictoc::toc()
+
+  # Conditionally return 'err', if any errors at this point
+  if (isTRUE(length(err) > 0)) {return(err)}
+
+  # * do max-element values make sense with value sets?
+  cat("do max-element values make sense with value sets?...")
+  tictoc::tic()
+  is_valid_valset <- purrr::map_lgl(obj, function(x) {
+
+    valset_x <- purrr::reduce(x, `c`)
+    unq_valset_x_cnt <- length(valset_x)
+    max_valset_x <- max(valset_x)
+
+    res <- isTRUE(identical(as.integer(sort(valset_x)), as.integer(1:unq_valset_x_cnt)))
+    return(res)
+
+  })
+  if (!isTRUE(all(is_valid_valset))) {
+    msg <- "`obj` elements must have valid column values"
+    err <- c(msg, err)
+  }
+  tictoc::toc()
+
+  # Return Final Output
+  if (isTRUE(length(err) == 0)) {return(obj)}
+  else {return(err)}
+
+}
+
+#' Validate if an input object is 'pl_records'
+#'
+#' @param obj S3 Object
+#'
+#' @return R Object
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' test <- valid_pl_records(obj)
+#' }
+valid_pl_records <- function(obj) {
+
+  # Validate Inputs
+  if (missing(obj)) {stop("`obj` is missing in call to `valid_pl_records`")}
+
+  # Initialize Empty Character Vector for Error Messages
+  err <- vector(mode = 'character')
+
+  # Validate Input Expectations
+
+  # * is data.frame?
+  cat("is data.frame?...")
+  tictoc::tic()
+  if (!isTRUE(is.list(obj))) {
+    msg <- "`obj` must be a data.frame"
+    err <- c(msg, err)
+  }
+  tictoc::toc()
+
+  # * does data.frame have colnames?
+  cat("does data.frame have colnames?...")
+  tictoc::tic()
+  obj_names <- colnames(obj)
+  obj_names_char <- isTRUE(is.character(obj_names))
+  obj_names_na <- purrr::map_lgl(obj_names, function(x){isTRUE(is.na(x))})
+  obj_names_null <- purrr::map_lgl(obj_names, function(x){isTRUE(is.null(x))})
+  obj_names_empty <- isTRUE(any(obj_names_na)) || isTRUE(any(obj_names_null))
+  if (!isTRUE(obj_names_char) && !isTRUE(obj_names_empty)) {
+    msg <- "`names(obj)` must be character vector with no NA/NULL elements"
+    err <- c(msg, err)
+  }
+  tictoc::toc()
+
+  # Conditionally return 'err', if any errors at this point
+  if (isTRUE(length(err) > 0)) {return(err)}
+
+  # * are all columns 'integer'?
+  cat("are all columns 'integer'?...")
+  tictoc::tic()
+  is_col_integer <- purrr::map_lgl(obj, function(x) {isTRUE(is.integer(x))})
+  if (!isTRUE(all(is_col_integer))) {
+    msg <- "`obj` must only contain integer columns"
+    err <- c(msg, err)
+  }
+  tictoc::toc()
+
+  # * are all column values non-empty?
+  cat("are all column values non-empty?...")
+  tictoc::tic()
+  is_non_empty <- purrr::map_lgl(obj, function(x){
+    isTRUE(any(purrr::map_lgl(x, function(t){
+      !isTRUE(is.na(t)) && !isTRUE(is.null(t))
+    })))
+  })
+  if (!isTRUE(all(is_non_empty))) {
+    msg <- "`obj` columns cannot contain empty cells"
+    err <- c(msg, err)
+  }
+  tictoc::toc()
+
+  # * are distinct values 'dense'?
+  cat("are distinct values 'dense'?...")
+  tictoc::tic()
+  is_val_dense <- purrr::map_lgl(obj, function(x){
+    unq_val_x <- unique(x)
+    res <- isTRUE(identical(as.integer(sort(unq_val_x)), as.integer(1:length(unq_val_x))))
+    return(res)
+  })
+  if (!isTRUE(all(is_val_dense))) {
+    msg <- "`obj` column values must all be dense"
+    err <- c(msg, err)
+  }
+  tictoc::toc()
+
+  # Return Final Output
+  if (isTRUE(length(err) == 0)) {return(obj)}
+  else {return(err)}
+
+}
