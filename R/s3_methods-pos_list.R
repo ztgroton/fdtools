882387083b3faf7e14f91b2e5910895c82cbda65
ztgroton <ztgroton@gmail.com>
2023-02-19 11:43:34 -0500

    Modified logic for S3 Method 'strip_part'.

--- R/s3_methods-pos_list.R
+++ R/s3_methods-pos_list.R
@@ -2,7 +2,7 @@
 #' S3 Generic 'strip_part'
 #'
 #' @param obj S3 Object
-#' @param ... ellipsis
+#' @param col character - Vector of column names used to create partition
 #'
 #' @return list
 #' @export
@@ -11,12 +11,12 @@
 #' \dontrun{
 #' test <- strip_part(pos_list)
 #' }
-strip_part <- function(obj, ...) {UseMethod("strip_part", obj)}
+strip_part <- function(obj, col) {UseMethod("strip_part", obj)}
 
 #' S3 Generic 'strip_part' returns a stripped partition
 #'
 #' @param obj S3 Object
-#' @param ... ellipsis
+#' @param col character - Vector of column names used to create partition
 #'
 #' @return list
 #' @export
@@ -25,14 +25,31 @@ strip_part <- function(obj, ...) {UseMethod("strip_part", obj)}
 #' \dontrun{
 #' test <- strip_part.pos_list(pos_list)
 #' }
-strip_part.pos_list <- function(obj, ...) {
+strip_part.pos_list <- function(obj, col) {
 
   # Validate Inputs
   if (missing(obj)) {stop("`obj` is missing in call to `strip_part.pos_list`")}
+  if (missing(col)) {stop("`col` is missing in call to `strip_part_from_record`")}
 
   # Validate Input Expectations
+
+  # * `obj`
   if (!isTRUE(inherits(obj, 'pos_list'))) {
     stop("`obj` must inherit from 'pos_list' in call to `strip_part.pos_list`")
   }
+
+  # * `col`
+  col_in_map <- isTRUE(all(col %in% names(obj$data$pl$map)))
+  col_in_index <- isTRUE(all(col %in% names(obj$data$pl$index)))
+  col_in_records <- isTRUE(all(col %in% names(obj$data$pl$records)))
+  if (!isTRUE(col_in_map) || !isTRUE(col_in_index) || !isTRUE(col_in_records)) {
+    stop("`col` must be a subset of columns in `obj` in call to `strip_part_from_record`")
+  }
+
+  # Compute Stripped Partition
+  result <- strip_part_from_record(pos = obj, col = col)
+
+  # Return Result
+  return(result)
 
 }
