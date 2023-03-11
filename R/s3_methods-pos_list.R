9a34b2ffc05debdf87a462a54828594e8c9c9545
ztgroton <ztgroton@gmail.com>
2023-03-11 16:10:56 -0500

    Implemented logic for S3 Methods 'key_err' and 'fd_err'.

--- R/s3_methods-pos_list.R
+++ R/s3_methods-pos_list.R
@@ -53,3 +53,86 @@ strip_part.pos_list <- function(obj, col) {
   return(result)
 
 }
+
+#' S3 Generic 'key_err'
+#'
+#' @param obj S3 Object
+#' @param col character - Vector of column names used to create partition
+#' @param err_only TRUE/FALSE - Optionally specify if only failing values should be returned
+#'
+#' @return list
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' test <- strip_part(pos_list)
+#' }
+key_err <- function(obj, col, err_only) {UseMethod("key_err", obj)}
+
+#' S3 Generic 'key_err' returns a stripped partition
+#'
+#' @param obj S3 Object
+#' @param col character - Vector of column names used to create partition
+#' @param err_only TRUE/FALSE - Optionally specify if only failing values should be returned
+#'
+#' @return list
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' test <- strip_part.pos_list(pos_list)
+#' }
+key_err.pos_list <- function(obj, col, err_only) {
+
+  # Validate Inputs
+  if (missing(obj)) {stop("`obj` is missing in call to `key_err.pos_list`")}
+  if (missing(col)) {stop("`col` is missing in call to `key_err.pos_list`")}
+  if (missing(err_only)) {err_only <- FALSE}
+
+  # Call `err_key_from_records`
+  err_key_from_records(pos = obj, col = col, err_only = err_only)
+
+}
+
+#' S3 Generic 'fd_err'
+#'
+#' @param obj S3 Object
+#' @param xcol character - Vector of column names used to define LHS of FD
+#' @param ycol character - Vector of column names used to define RHS of FD
+#' @param err_only TRUE/FALSE - Optionally specify if only failing values should be returned
+#'
+#' @return list
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' test <- strip_part(pos_list)
+#' }
+fd_err <- function(obj, xcol, ycol, err_only) {UseMethod("fd_err", obj)}
+
+#' S3 Generic 'key_err' returns a stripped partition
+#'
+#' @param obj S3 Object
+#' @param xcol character - Vector of column names used to define LHS of FD
+#' @param ycol character - Vector of column names used to define RHS of FD
+#' @param err_only TRUE/FALSE - Optionally specify if only failing values should be returned
+#'
+#' @return list
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' test <- strip_part.pos_list(pos_list)
+#' }
+fd_err.pos_list <- function(obj, xcol, ycol, err_only) {
+
+  # Validate Inputs
+  if (missing(obj)) {stop("`obj` is missing in call to `fd_err.pos_list`")}
+  if (missing(xcol)) {stop("`xcol` is missing in call to `fd_err.pos_list`")}
+  if (missing(ycol)) {stop("`ycol` is missing in call to `fd_err.pos_list`")}
+  if (missing(err_only)) {err_only <- FALSE}
+
+  # Call `err_fd_from_records`
+  err_fd_from_records(pos = obj, xcol = xcol, ycol = ycol, err_only = err_only)
+
+}
--- R/util-err_func.R
+++ R/util-err_func.R
@@ -34,6 +34,7 @@ err_key_from_strip_part <- function(part) {
 #'
 #' @param pos S3 Object of class 'pos_list'
 #' @param col character - Vector of column names
+#' @param err_only TRUE/FALSE - Optionally specify if only failing values should be returned
 #'
 #' @return R Object
 #'
@@ -41,11 +42,12 @@ err_key_from_strip_part <- function(part) {
 #' \dontrun{
 #' result <- err_key_from_records(pos_list, col_list)
 #' }
-err_key_from_records <- function(pos, col) {
+err_key_from_records <- function(pos, col, err_only) {
 
   # Validate Inputs
   if (missing(pos)) {stop("`pos` is missing in call to `err_key_from_records`")}
   if (missing(col)) {stop("`col` is missing in call to `err_key_from_records`")}
+  if (missing(err_only)) {err_only <- FALSE}
 
   # Validate Input Expectations
 
@@ -62,6 +64,11 @@ err_key_from_records <- function(pos, col) {
     stop("`col` must be a subset of columns in `pos` in call to `err_key_from_records`")
   }
 
+  # * `err_only`
+  if (!isTRUE(identical(err_only, TRUE)) && !isTRUE(identical(err_only, FALSE))) {
+    stop("`err_only` must be TRUE/FALSE in call to `err_key_from_records`")
+  }
+
   # Nest Row Numbers using Distinct Values from 'col'
   data <- pos$data$pl$records %>%
     dplyr::select(dplyr::all_of(col)) %>%
@@ -71,8 +78,10 @@ err_key_from_records <- function(pos, col) {
   # Count Nested Rows
   data <- data %>% dplyr::mutate(orig_row_cnt = purrr::map_dbl(.data$orig_row_num, function(x){nrow(x)}))
 
-  # Filter Out Nested Row Groups with only one record
-  data <- data %>% dplyr::filter(.data$orig_row_cnt > 1)
+  # Conditionally Filter Out Nested Row Groups with only one record
+  if (isTRUE(err_only)) {
+    data <- data %>% dplyr::filter(.data$orig_row_cnt > 1)
+  }
 
   # Calculate Sum of the Size of Equivalence Classes in 'part'
   size_sum <- sum(data$orig_row_cnt)
@@ -93,7 +102,7 @@ err_key_from_records <- function(pos, col) {
 #'
 #' @examples
 #' \dontrun{
-#' result <- err_fd_from_strip_part(strip_part)
+#' result <- err_fd_from_strip_part(x_part, xy_part)
 #' }
 err_fd_from_strip_part <- function(x_part, xy_part) {
 
@@ -117,20 +126,23 @@ err_fd_from_strip_part <- function(x_part, xy_part) {
 #' Calculate 'Functional Dependency Error' from Position List Records
 #'
 #' @param pos S3 Object of class 'pos_list'
-#' @param x_col character - Vector of column names
-#' @param y_col character - Vector of column names
+#' @param xcol character - Vector of column names
+#' @param ycol character - Vector of column names
+#' @param err_only TRUE/FALSE - Optionally specify if only failing values should be returned
 #'
 #' @return R Object
 #'
 #' @examples
 #' \dontrun{
-#' result <- err_key_from_records(strip_part)
+#' result <- err_key_from_records(pos_list, xcol, ycol)
 #' }
-err_fd_from_records <- function(pos, x_col, y_col) {
+err_fd_from_records <- function(pos, xcol, ycol, err_only) {
 
   # Validate Inputs
   if (missing(pos)) {stop("`pos` is missing in call to `err_fd_from_records`")}
-  if (missing(col)) {stop("`col` is missing in call to `err_fd_from_records`")}
+  if (missing(xcol)) {stop("`xcol` is missing in call to `err_fd_from_records`")}
+  if (missing(ycol)) {stop("`ycol` is missing in call to `err_fd_from_records`")}
+  if (missing(err_only)) {err_only <- FALSE}
 
   # Validate Input Expectations
 
@@ -139,12 +151,52 @@ err_fd_from_records <- function(pos, x_col, y_col) {
     stop("`pos` must inherit from 'pos_list' in call to `err_fd_from_records`")
   }
 
-  # * `col`
-  col_in_map <- isTRUE(all(col %in% names(pos$data$pl$map)))
-  col_in_index <- isTRUE(all(col %in% names(pos$data$pl$index)))
-  col_in_records <- isTRUE(all(col %in% names(pos$data$pl$records)))
-  if (!isTRUE(col_in_map) || !isTRUE(col_in_index) || !isTRUE(col_in_records)) {
-    stop("`col` must be a subset of columns in `pos` in call to `err_fd_from_records`")
+  # * `xcol`
+  xcol_in_map <- isTRUE(all(xcol %in% names(pos$data$pl$map)))
+  xcol_in_index <- isTRUE(all(xcol %in% names(pos$data$pl$index)))
+  xcol_in_records <- isTRUE(all(xcol %in% names(pos$data$pl$records)))
+  if (!isTRUE(xcol_in_map) || !isTRUE(xcol_in_index) || !isTRUE(xcol_in_records)) {
+    stop("`xcol` must be a subset of columns in `pos` in call to `err_fd_from_records`")
   }
+
+  # * `ycol`
+  ycol_in_map <- isTRUE(all(ycol %in% names(pos$data$pl$map)))
+  ycol_in_index <- isTRUE(all(ycol %in% names(pos$data$pl$index)))
+  ycol_in_records <- isTRUE(all(ycol %in% names(pos$data$pl$records)))
+  if (!isTRUE(ycol_in_map) || !isTRUE(ycol_in_index) || !isTRUE(ycol_in_records)) {
+    stop("`ycol` must be a subset of columns in `pos` in call to `err_fd_from_records`")
+  }
+
+  # * `xcol` & `ycol`
+  if (!isTRUE(length(intersect(xcol, ycol)) == 0)) {
+    stop("`xcol` and `ycol` must be disjoint in call to `err_fd_from_records`")
+  }
+
+  # * `err_only`
+  if (!isTRUE(identical(err_only, TRUE)) && !isTRUE(identical(err_only, FALSE))) {
+    stop("`err_only` must be TRUE/FALSE in call to `err_fd_from_records`")
+  }
+
+  # Nest Row Numbers using Distinct Values from `union(xcol,ycol)`
+  data <- pos$data$pl$records %>%
+    dplyr::select(dplyr::all_of(union(xcol,ycol))) %>%
+    dplyr::mutate(xy_row_num = dplyr::row_number()) %>%
+    tidyr::nest(orig_xy_row_num = c(.data$xy_row_num))
+
+  # Count Nested Rows
+  data <- data %>% dplyr::mutate(orig_xy_row_cnt = purrr::map_dbl(.data$orig_xy_row_num, function(x){nrow(x)}))
+
+  # Nest Columns in `data` using Distinct Values from `xcol`
+  data <- data %>% tidyr::nest(.by = dplyr::all_of(xcol), .key = 'yval')
+
+  # Count Nested Rows
+  data <- data %>% dplyr::mutate(yval_cnt = purrr::map_dbl(.data$yval, function(x){nrow(x)}))
+
+  # Conditionally Filter Out Nested Row Groups with only one record
+  if (isTRUE(err_only)) {
+    data <- data %>% dplyr::filter(.data$yval_cnt > 1)
+  }
+
+  return(data)
 
 }
